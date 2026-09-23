bb376d3ce84d0e6bd1dfee2316c70f05aed65d02
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-09-23 10:19:42 +0200

    fix communication with customer-service ui

--- src/main/java/com/example/guesthousebookingsystem/controllers/HomeController.java
+++ src/main/java/com/example/guesthousebookingsystem/controllers/HomeController.java
@@ -1,12 +1,22 @@
 package com.example.guesthousebookingsystem.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private final String customerServiceBaseUrl;
+
+    public HomeController(@Value("${customer-service.base-url}") String customerServiceBaseUrl) {
+        this.customerServiceBaseUrl = customerServiceBaseUrl;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("customerServiceBaseUrl", customerServiceBaseUrl);
         return "index";
     }
 }
